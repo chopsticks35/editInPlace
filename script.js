37ec781ae6f2ee7c1a56d78efc0b52d6a0c59053
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-01-21 17:01:37 -0700

    tired

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 angular.module('myApp', []);
 
-angular.module('myApp)
+angular.module('myApp')
         
-               .controller('myController, [
-                           
-                           
-    ]
+        .controller('myController', ['$scope', function($scope) {
+            $scope.inputHide = function($event){
+            $scope.hide = true;
+                console.log('in function');
+           }
+        }])
